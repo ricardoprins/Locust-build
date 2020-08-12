8696a35228c9c3ce7336ab5d216debbf59bb5fce
anushkrishnav <anush.venkatakrishna@gmail.com>
2020-08-12 17:49:25 +0530

    .

--- app/app.py
+++ app/app.py
@@ -6,15 +6,13 @@ from .extensions import db
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 
-def create_app(config_object=settings):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(config_object)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object(config_object)
 
-    register_extensions(app)
-    register_blueprints(app)
-    register_errorhandlers(app)
-    return app
+#    register_extensions(app)
+register_blueprints(app)
+register_errorhandlers(app)
+
 '''
 def register_extensions(app):
     """Register Flask extensions."""
